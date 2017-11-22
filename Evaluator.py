9cf802b28717b5cfb66e495b71055c6cc299d8eb
Tony Peng <tonypeng@mit.edu>
2017-11-22 03:23:32 +0000

    add weighted ensembling to evaluation

--- Evaluator.py
+++ Evaluator.py
@@ -3,12 +3,13 @@ import tensorflow as tf
 from DataLoader import *
 
 class Evaluator:
-    def __init__(self, arch, model_name, model_path, data_root, data_list,
-                    load_size, fine_size, batch_size, device, data_mean,
-                    hidden_activation):
+    def __init__(self, arch, model_names, model_paths, model_weights, data_root,
+                    data_list, load_size, fine_size, batch_size, device,
+                    data_mean, hidden_activation):
         self.arch = arch
-        self.model_name = model_name
-        self.model_path = model_path
+        self.model_names = model_names
+        self.model_paths = model_paths
+        self.model_weights = model_weights
         self.data_root = data_root
         self.data_list = data_list
         self.load_size = load_size
@@ -31,6 +32,14 @@ class Evaluator:
 
         loader = DataLoaderDisk(**opt_data_eval)
 
+        total_output = np.zeros(100)
+
+        model_names = self.model_names.split(',')
+        model_paths = self.model_paths.split(',')
+        archs = self.arch.split(',')
+        model_weights = map(int, self.model_weights.split(','))
+        assert len(model_names) == len(model_paths) == len(archs)
+
         g = tf.Graph()
         with g.as_default(), g.device(self.device), tf.Session(
                 config=tf.ConfigProto(allow_soft_placement=True)) as sess:
@@ -39,46 +48,53 @@ class Evaluator:
             y = tf.placeholder(tf.int64, None)
             is_training = tf.placeholder(tf.bool, name='is_training')
 
-            with tf.variable_scope(self.model_name):
-                net = self._construct_net(x, is_training)
+            nets = []
+            for model_name, model_weight, arch in zip(model_names,
+                                                        model_weights,
+                                                        archs):
+                with tf.variable_scope(model_name):
+                    nets.append(self._construct_net(
+                                        arch, x, is_training) * model_weight)
+            combined = tf.add_n(nets)
 
             sess.run(tf.global_variables_initializer())
 
-            top5 = tf.nn.top_k(net, k=5, sorted=True)
-            acc5 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(net, y, 5), tf.float32)) * 100
+            top5 = tf.nn.top_k(combined, k=5, sorted=True)
+            acc5 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(combined, y, 5), tf.float32)) * 100
 
             # Restore model
-            saver = tf.train.Saver(max_to_keep=5)
-            saver.restore(sess, self.model_path)
+            for model_name, model_path in zip(model_names, model_paths):
+                saver = tf.train.Saver(max_to_keep=5, var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=model_name))
+                saver.restore(sess, model_path)
 
             img_num = 1
             while img_num <= loader.size():
                 images_batch, labels_batch = loader.up_to(self.batch_size)
                 # Evaluate
-                top5_res, acc5_res = sess.run([top5, acc5], feed_dict={
+                top5_output, acc5_output = sess.run([top5, acc5], feed_dict={
                                         x: images_batch,
                                         y: labels_batch,
                                         is_training: False})
-
-                for particular_top_5 in top5_res.indices:
+                for particular_top_5 in top5_output.indices:
                     print('test/'+str(img_num).zfill(8)+'.jpg '+(' '.join(str(c) for c in particular_top_5)))
                     img_num += 1
 
-    def _construct_net(self, inp, is_training):
-        if self.arch == 'alexnet':
+
+    def _construct_net(self, arch, inp, is_training):
+        if arch == 'alexnet':
             return nets.AlexNet(inp, 1.0)
-        elif self.arch == 'inception':
+        elif arch == 'inception':
             logits, end_points = inception_v4.inception_v4(inp, num_classes=100, dropout_keep_prob=1.0)
             return logits
-        elif self.arch == 'resnet18':
+        elif arch == 'resnet18':
             return nets.ResNet18(inp, is_training, {
                         'hidden_activation': self.hidden_activation
                         })
-        elif self.arch == 'resnet34':
+        elif arch == 'resnet34':
             return nets.ResNet34(inp, is_training, {
                         'hidden_activation': self.hidden_activation
                         })
-        elif self.arch == 'resnet50':
+        elif arch == 'resnet50':
             return nets.ResNet50(inp, is_training, {
                         'hidden_activation': self.hidden_activation
                         })
--- evaluate.py
+++ evaluate.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser(description='')
 parser.add_argument('--arch', type=str, default='resnet34')
 parser.add_argument('--model_names', type=str, default='')
 parser.add_argument('--model_paths', type=str, default='')
+parser.add_argument('--model_weights', type=str, default='')
 parser.add_argument('--data_root', type=str, default='data/images')
 parser.add_argument('--data_list', type=str, default='data/test.txt')
 parser.add_argument('--load_size', type=int, default=128)
@@ -19,6 +20,7 @@ args = parser.parse_args()
 data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842]) if args.center_mean else np.zeros(3)
 
 evaluator = Evaluator(args.arch, args.model_names, args.model_paths,
+                        args.model_weights,
                         args.data_root, args.data_list,
                         args.load_size, args.fine_size,
                         args.batch_size, args.device, data_mean,
